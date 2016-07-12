c50cda69c97cdc2630ce71c5d0f79a383eed0faa
Kevin Gray <Kevin@GoPosse.com>
2016-07-12 14:37:16 -0400

    initial commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'haitchTests' do
+  pod 'SnapKit', '0.21.1'
+  pod 'Haitch', '~> 0.7'
+end
---
+++ haitchTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  haitchTests
+//
+//  Created by Kevin Gray on 7/12/16.
+//  Copyright © 2016 posse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
